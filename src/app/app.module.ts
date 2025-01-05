2a2ecbffb90fe2694c4ff8bb31f694fe8e9bf9ac
T-Said-c <t.said.acc@gmail.com>
2025-01-05 20:14:40 +0100

    signup and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HTTP_INTERCEPTORS,HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders,HttpClientModule } from '@angular/common/http';
+import { BehaviorSubject, Observable, catchError, tap } from 'rxjs';
+
+
+export interface UserRegistration {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface RegistrationResponse {
+  success: boolean;
+  message: string;
+  user?: any;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  email: string;
+  fullName: string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/user';
+  private headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
+  private currentUserSubject = new BehaviorSubject<LoginResponse | null>(null);
+  public currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    const savedUser = localStorage.getItem('currentUser');
+    if (savedUser) {
+      this.currentUserSubject.next(JSON.parse(savedUser));
+    }
+  }
+
+  register(user: UserRegistration): Observable<RegistrationResponse> {
+    return this.http.post<RegistrationResponse>(
+      `${this.apiUrl}/registration`,
+      user,
+      { headers: this.headers }
+    ).pipe(
+      tap(response => console.log('Registration response:', response)),
+      catchError(error => {
+        console.error('Registration error:', error);
+        throw error;
+      })
+    );
+  }
+
+  login(credentials: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, credentials,
+      { headers: this.headers })
+      .pipe(
+        tap(response => {
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          this.currentUserSubject.next(response);
+        }),
+      catchError(error => {
+        console.error('Registration error:', error);
+        throw error;
+      })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.currentUserSubject.value;
+  }
+
+  getToken(): string | null {
+    return this.currentUserSubject.value?.token || null;
+  }
+
+}
